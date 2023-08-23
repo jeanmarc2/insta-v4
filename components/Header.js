a40dbb1ad97609df66567befc2b7f5e75ca01436
jean marc <jeanmarckk20@gmail.com>
2023-08-23 17:49:05 +0200

    add the color of the search bar and position it inside the searchBar.

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Image from 'next/image'
-
+import { SearchIcon } from '@heroicons/react/outline'
 
 export default function Header() {
   return (
-      <div>
-          {/**Left */}
-          <div className="flex items-center justify-between max-w-6xl">
+      
+      
+          
+      <div className="flex items-center justify-between max-w-6xl">
+          {/**Left */ }
           <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
               <Image
             src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
@@ -23,18 +25,20 @@ export default function Header() {
                      layout="fill"
             className="object-contain"
                   />
-              </div>
-
-              <h1>Right Sides</h1>
-              </div>
-          
+          </div>
           {/**Middle */}
-
-
+          <div className="relative mt-1">
+              <div className="absolute top-2 left-2">
+                  <SearchIcon className="h-5 text-gray-500"/>
+              </div>
+              <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black
+               focus:border-black rounded-md"/>
+          </div>
 
           {/**Right */}
 
-
-    </div>
+              <h1>Right Sides</h1>
+              </div>
+              
   )
 }
